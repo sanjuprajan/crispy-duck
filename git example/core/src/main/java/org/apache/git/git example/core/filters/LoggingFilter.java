b6b92c012daf215292627d5bb436fdd9e9b7b6e3
sanjupr <sanjupr@SANJUPR-W7-1.corp.adobe.com>
2015-08-26 16:28:16 +0200

    qw

--- git example/core/src/main/java/org/apache/git/git example/core/filters/LoggingFilter.java
+++ git example/core/src/main/java/org/apache/git/git example/core/filters/LoggingFilter.java
@@ -33,7 +33,7 @@ import org.slf4j.LoggerFactory;
 /**
  * Simple servlet filter component that logs incoming requests.
  */
-@SlingFilter(order = -703, scope = SlingFilterScope.REQUEST)
+@SlingFilter(order = -704, scope = SlingFilterScope.REQUEST)
 public class LoggingFilter implements Filter {
 
     private final Logger logger = LoggerFactory.getLogger(getClass());
